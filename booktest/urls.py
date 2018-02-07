cb18f6ed018798181c80aaff18c1a675ab709a42
yiran <1976967579@qq.com>
2018-02-07 22:14:14 +0800

    完善

--- booktest/urls.py
+++ booktest/urls.py
@@ -2,7 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(\d+)$', views.show, name='show'),
+    url(r'^index2$', views.index2, name='index2'),
+    url(r'^user1$', views.user1, name='user1'),
+    url(r'^user2$', views.user2, name='user2'),
+    url(r'^htmlTest$', views.htmlTest, name='htmlTest'),
+    url(r'^csrf1$', views.csrf1, name='csrf1'),
+    url(r'^csrf2$', views.csrf2, name='csrf2'),
 ]
 
 app_name = 'booktest'
--- booktest/views.py
+++ booktest/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import HeroInfo
 
 
@@ -8,5 +9,34 @@ def index(request):
     #context = {'hero': hero}
     #context = {}
     list = HeroInfo.objects.all()
-    context = {'list':list}
+    context = {'list': list}
     return render(request, 'booktest/index.html', context)
+
+def show(request,id):
+    context = {'id': id}
+    return render(request, 'booktest/show.html', context)
+
+#用于练习模板的继承
+def index2(request):
+    return render(request, 'booktest/index2.html')
+
+def user1(request):
+    context = {'uname':'chunjue'}
+    # user1得到的参数 可以显示到它的父级里 在base里{{ uname }}
+    return render(request, 'booktest/user1.html', context)
+
+def user2(request):
+    return render(request, 'booktest/user2.html')
+
+#html转义
+def htmlTest(request):
+    context = {'t1':'<h1>123</h1>'}
+    return render(request, 'booktest/htmlTest.html', context)
+
+#CSRF 跨站请求伪造
+def csrf1(request):
+    return render(request, 'booktest/csrf1.html')
+
+def csrf2(request):
+    uname = request.POST['uname']
+    return HttpResponse(uname)
